e6d81f537dd6bad6f2503a292f1f0648809c1fc5
ashok270587 <30891992+ashok270587@users.noreply.github.com>
2022-01-18 15:01:30 +0530

    Add files via upload

---
+++ SampleProject.java
@@ -0,0 +1,10 @@
+public class Mathematics {
+	//class level variable
+	public static String emailId = "test123@gmail.com";
+
+	public boolean validateEmail(String expEmailId){
+		if(str1.equals(str2)
+	}
+
+
+}
